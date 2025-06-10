6510874140484d48d1542580d832d354e6bfd645
Justin Leung <justin.leung124@gmail.com>
2025-06-09 17:02:04 -0700

    created s3 upload

---
+++ database/s3_client.py
@@ -0,0 +1,46 @@
+import boto3
+import gzip
+import io
+
+
+def upload_html(html_data: str, object_key: str, bucket_name: str):
+    s3_client = boto3.client("s3")
+    compressed_data = compress_html(html_data)
+    try:
+        s3_client.upload_fileobj(
+            compressed_data,
+            bucket_name,
+            object_key,
+            ExtraArgs={
+                'ContentType': 'text/html',
+                'ContentEncoding': 'gzip'
+            }
+        )
+        print(f"Uploaded to '{bucket_name}/{object_key}' successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+def compress_html(html_data: str):
+    compressed_buffer = io.BytesIO()
+    with gzip.GzipFile(fileobj=compressed_buffer, mode='wb') as gz:
+        gz.write(html_data.encode('utf-8'))
+    compressed_buffer.seek(0)
+
+    return compressed_buffer
+
+def get_html(bucket_name: str, date: str) -> dict:
+    s3_client = boto3.client("s3")
+    paginator = s3_client.get_paginator('list_objects_v2')
+    prefix = f"{date}/"
+    html_data = {}
+
+    for page in paginator.paginate(Bucket=bucket_name, Prefix=prefix):
+        for obj in page.get('Contents', []):
+            key = obj['Key']
+
+            response = s3_client.get_object(Bucket=bucket_name, Key=key)
+            with gzip.GzipFile(fileobj=io.BytesIO(response['Body'].read())) as gz:
+                html_str = gz.read().decode('utf-8')
+                html_data[key] = html_str
+
+    return html_data
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -4,10 +4,13 @@ import random
 import time
 
 from bs4 import BeautifulSoup
+from datetime import date
 
 from helpers.read_config import read_config
+from database.s3_client import upload_html
 
 
+S3_BUCKET_NAME = 'linkedin-jobs-data'
 proxy_config = read_config("oxylabs")
 
 def get_proxy():
@@ -32,7 +35,7 @@ def scrape_jobs(pages: int):
     opener.addheaders = [("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")]
     
     for i in range(0, pages * 25, 25):
-        url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Software%20Engineer&location=United%20States&start={i}&f_TPR=r604800"
+        url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Software%20Engineer&location=United%20States&start={i}&f_TPR=r86400"
         response = opener.open(url)
         scrape_page(response.read(), opener)
         time.sleep(random.uniform(2, 5))
@@ -45,9 +48,12 @@ def scrape_page(html: str, opener: OpenerDirector):
             job_id = urn.split(':')[-1]
             url = f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{job_id}"
             job_response = opener.open(url)
+            html_bytes = job_response.read()
+            html_str = html_bytes.decode('utf-8')
 
-            # push to S3
-        
+            upload_html(html_str, f"{date.today()}/{job_id}", S3_BUCKET_NAME)
+
         time.sleep(random.uniform(2, 5))
 
-scrape_jobs(5)
+if __name__ == "__main__":
+    scrape_jobs(1)
