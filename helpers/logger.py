b204cf7f6140acff2fd61d919b29e761c807e3f0
Justin Leung <justin.leung124@gmail.com>
2025-06-11 03:55:37 -0700

    update logger for aws glue

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,14 +1,9 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger('scraping_logger')
 logger.setLevel(logging.INFO)
 
-# Rotate when file reaches 5 MB, keep 3 backups
-handler = RotatingFileHandler('scraping.log', maxBytes=5*1024*1024, backupCount=3)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
