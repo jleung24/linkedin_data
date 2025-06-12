1be6aa3cef11b4c04f66101d2b19e43d1726068c
Justin Leung <justin.leung124@gmail.com>
2025-06-12 03:33:11 -0700

    update

--- database/migrations/migration_1.sql
+++ database/migrations/migration_1.sql
@@ -1,12 +1,12 @@
 CREATE TABLE jobs (
-    job_id INTEGER PRIMARY KEY,
+    job_id BIGINT PRIMARY KEY,
     title VARCHAR(100),
     company VARCHAR(50),
     location VARCHAR(50),
     level VARCHAR(50),
     years_experience_min INTEGER,
     years_experience_max INTEGER,
-    url VARCHAR(200)
+    url VARCHAR(200),
     posted_date DATE
 );
 
@@ -17,7 +17,7 @@ CREATE TABLE skills (
 
 CREATE TABLE salary (
     salary_id INTEGER IDENTITY(1,1) PRIMARY KEY,
-    job_id INTEGER NOT NULL REFERENCES jobs(job_id),
+    job_id BIGINT NOT NULL REFERENCES jobs(job_id),
     amount_min NUMERIC(12,2) NOT NULL,
     amount_max NUMERIC(12,2) NOT NULL,
     time_unit VARCHAR(10) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE salary (
 );
 
 CREATE TABLE job_skills (
-    job_id INTEGER NOT NULL,
+    job_id BIGINT NOT NULL,
     skill_id INTEGER NOT NULL,
     PRIMARY KEY (job_id, skill_id),
     FOREIGN KEY (job_id) REFERENCES jobs(job_id),
