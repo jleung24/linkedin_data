f233545663bbdeddb303da61535a52e0459ed9f1
Justin <justin.leung124@gmail.com>
2025-06-18 18:04:23 -0700

    implement bulk load for job inserts

--- glue_jobs/parser_job.py
+++ glue_jobs/parser_job.py
@@ -30,9 +30,11 @@ except Exception as e:
     traceback.print_exc()
     raise
 
+import psycopg2
+import pandas as pd
+import boto3
 from linkedin_data.scraping.parser import process_s3
 from linkedin_data.helpers.read_config import read_config
-import psycopg2
 
 rds_config = read_config('redshift')
 yesterday = date.today() - timedelta(days=1)
@@ -48,44 +50,96 @@ conn = psycopg2.connect(
     user=redshift_user,
     password=redshift_password
 )
-cur = conn.cursor()
 
+cur = conn.cursor()
 cur.execute("SELECT skill_id, name FROM skills")
 skills_rows = cur.fetchall()
 skill_name_to_id = {row[1]: row[0] for row in skills_rows}
+cur.close()
+conn.close()
+
+jobs_data = []
+salary_data = []
+job_skills_data = []
 
 for job_id, job in parsed_job_data.items():
-    try:
-        # Insert job
-        cur.execute("""
-            INSERT INTO jobs (job_id, title, company, location, level, years_experience_min, years_experience_max, url, posted_date)
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
-        """, (
-            job_id, job["title"], job["company"], job["location"], job["level"],
-            job["years_experience_min"], job["years_experience_max"], job["url"], job["date"]
-        ))
-        
-        # Insert salary
-        if job["salary_min"] is not None:
-            cur.execute("""
-                INSERT INTO salary (job_id, amount_min, amount_max, time_unit, currency)
-                VALUES (%s,%s,%s,%s,%s)
-            """, (
-                job_id, job["salary_min"], job["salary_max"], job["salary_unit"], "USD"
-            ))
-    
-        # Insert job_skills
-        for skill in job["found_skills"]:
-            skill_id = skill_name_to_id.get(skill)
-            if skill_id:
-                cur.execute("""
-                    INSERT INTO job_skills (job_id, skill_id)
-                    VALUES (%s, %s)
-                """, (job_id, skill_id))
-    except Exception as e:
-        print(f"Error inserting job_id {job_id}: {e}")
-        conn.rollback()
+    jobs_data.append([
+        job_id, job["title"], job["company"], job["location"], job["level"],
+        job["years_experience_min"], job["years_experience_max"], job["url"], job["date"]
+    ])
+    if job["salary_min"] is not None:
+        salary_data.append([
+            job_id, job["salary_min"], job["salary_max"], job["salary_unit"], "USD"
+        ])
+    for skill in job["found_skills"]:
+        skill_id = skill_name_to_id.get(skill)
+        if skill_id:
+            job_skills_data.append([job_id, skill_id])
 
+jobs_df = pd.DataFrame(jobs_data, columns=[
+    'job_id', 'title', 'company', 'location', 'level',
+    'years_experience_min', 'years_experience_max', 'url', 'posted_date'
+])
+
+salary_df = pd.DataFrame(salary_data, columns=[
+    'job_id', 'amount_min', 'amount_max', 'time_unit', 'currency'
+])
+
+job_skills_df = pd.DataFrame(job_skills_data, columns=['job_id', 'skill_id'])
+
+s3 = boto3.client('s3')
+bucket = 'temp817'
+prefix = 'redshift_load/'
+
+jobs_csv = '/tmp/jobs.csv'
+jobs_df.to_csv(jobs_csv, index=False)
+s3.upload_file(jobs_csv, bucket, f'{prefix}jobs.csv')
+
+salary_csv = '/tmp/salary.csv'
+salary_df.to_csv(salary_csv, index=False)
+s3.upload_file(salary_csv, bucket, f'{prefix}salary.csv')
+
+job_skills_csv = '/tmp/job_skills.csv'
+job_skills_df.to_csv(job_skills_csv, index=False)
+s3.upload_file(job_skills_csv, bucket, f'{prefix}job_skills.csv')
+
+iam_role = 'arn:aws:iam::385122037390:role/service-role/AmazonRedshift-CommandsAccessRole-20250618T175826'  
+
+conn = psycopg2.connect(
+    host="default-workgroup.385122037390.us-west-1.redshift-serverless.amazonaws.com",
+    port=5439,
+    dbname="job_data",
+    user=rds_config['username'],
+    password=rds_config['password']
+)
+cur = conn.cursor()
+
+copy_jobs = f"""
+COPY jobs
+FROM 's3://{bucket}/{prefix}jobs.csv'
+IAM_ROLE '{iam_role}'
+FORMAT AS CSV
+IGNOREHEADER 1;
+"""
+copy_salary = f"""
+COPY salary
+FROM 's3://{bucket}/{prefix}salary.csv'
+IAM_ROLE '{iam_role}'
+FORMAT AS CSV
+IGNOREHEADER 1;
+"""
+copy_job_skills = f"""
+COPY job_skills
+FROM 's3://{bucket}/{prefix}job_skills.csv'
+IAM_ROLE '{iam_role}'
+FORMAT AS CSV
+IGNOREHEADER 1;
+"""
+
+cur.execute(copy_jobs)
+cur.execute(copy_salary)
+cur.execute(copy_job_skills)
 conn.commit()
 cur.close()
 conn.close()
+
